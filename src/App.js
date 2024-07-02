0d37e9ab33ae6bfa36724e57173a4dce4fdce967
FenixcoderX <fenixcoder2024@gmail.com>
2024-07-02 13:58:57 +0700

    Initial commit with base setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  return (
+    <div className="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-[calc(10px+2vmin)] text-white">
+     tariff calcolator
+    </div>
+  );
+}
+
+export default App;
